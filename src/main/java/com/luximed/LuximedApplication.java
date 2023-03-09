7f4e8da0638d564b0ef4d58ac2b318d0085c338e
ImperTator644 <ukaczyski6@gmail.com>
2023-03-09 19:52:09 +0100

    First commit

---
+++ src/main/java/com/luximed/LuximedApplication.java
@@ -0,0 +1,13 @@
+package com.luximed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuximedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuximedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luximed/LuximedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luximed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuximedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
