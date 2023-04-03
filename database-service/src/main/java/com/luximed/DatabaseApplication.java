f7d54a1ed75cc8182b870708ad0f00ceb2d90f90
ImperTator644 <ukaczyski6@gmail.com>
2023-04-03 17:11:19 +0200

    feature/kaczynl Divide project into microservices, added eureka

--- database-service/src/main/java/com/luximed/DatabaseApplication.java
+++ database-service/src/main/java/com/luximed/DatabaseApplication.java
@@ -2,12 +2,13 @@ package com.luximed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class LuximedApplication {
-
+@EnableEurekaClient
+public class DatabaseApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LuximedApplication.class, args);
+		SpringApplication.run(DatabaseApplication.class, args);
 	}
 
 }
