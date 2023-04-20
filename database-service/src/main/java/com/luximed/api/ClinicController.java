d679d5f693bcf17e7ce01b134a46122ef720fff5
patrykcyran <patrykcyranek@gmail.com>
2023-04-20 22:22:17 +0200

    Swagger added

--- database-service/src/main/java/com/luximed/api/ClinicController.java
+++ database-service/src/main/java/com/luximed/api/ClinicController.java
@@ -5,7 +5,7 @@ import com.luximed.repository.ClinicRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "api/database")
+@RequestMapping(value = "api/database/clinic")
 public class ClinicController {
 
     private final ClinicRepository clinicRepository;
@@ -14,7 +14,7 @@ public class ClinicController {
         this.clinicRepository = clinicRepository;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "add-clinic")
+    @RequestMapping(method = RequestMethod.POST, value = "add")
     public void addClinic(@RequestParam String buildingNumber,
                           @RequestParam String city,
                           @RequestParam String postalCode,
@@ -28,13 +28,13 @@ public class ClinicController {
         clinicRepository.save(clinic);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "get-clinic")
+    @RequestMapping(method = RequestMethod.GET, value = "get")
     public Clinic getClinicById(@RequestParam Integer id)
     {
         return clinicRepository.findById(id).orElse(null);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "delete-clinic")
+    @RequestMapping(method = RequestMethod.POST, value = "delete")
     public void deleteClinic(@RequestParam Integer id)
     {
         clinicRepository.deleteById(id);
