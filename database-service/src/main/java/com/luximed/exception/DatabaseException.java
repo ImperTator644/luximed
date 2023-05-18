ae57dff49b317430034d6094c308bcc92432d4f6
patrykcyran <patrykcyranek@gmail.com>
2023-05-18 22:22:39 +0200

    1. add clinic to doctor 2. add specialization to doctor 3. get all appointment types by specialization

--- database-service/src/main/java/com/luximed/exception/DatabaseException.java
+++ database-service/src/main/java/com/luximed/exception/DatabaseException.java
@@ -1,4 +1,11 @@
 package com.luximed.exception;
 
 public class DatabaseException extends RuntimeException {
+    public DatabaseException(String errorMessage){
+        super(errorMessage);
+    }
+
+    public DatabaseException(){
+        super("Database exception");
+    }
 }
--- database-service/src/main/java/com/luximed/repository/AppointmentTypeRepository.java
+++ database-service/src/main/java/com/luximed/repository/AppointmentTypeRepository.java
@@ -4,6 +4,9 @@ import com.luximed.model.AppointmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AppointmentTypeRepository extends JpaRepository<AppointmentType, Integer> {
+    List<AppointmentType> getAppointmentTypesBySpecializationId(Integer id);
 }
